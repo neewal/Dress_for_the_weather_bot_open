0c436d8909052548231bc6145d3ba72795956350
neewal <101584059+neewal@users.noreply.github.com>
2023-09-21 11:56:18 +0300

    Дополнение для main.py

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ import requests
 import json
 from bs4 import BeautifulSoup
 import random
+import logging
 
 good = ['хорошего', 'отличного', 'прекрастного', 'вдохновляющего', 'незабываемого', 'прелестного', 'доброго', 'удачного']
 
-API_TOKEN = "Token_BOT"
+#Логирование
+logging.basicConfig(level=logging.INFO, force=False, filename="main.log",
+                    format="%(asctime)s %(levelname)s %(message)s")
+
+API_TOKEN = "API_Token_BOT"
 
 bot = telebot.TeleBot(API_TOKEN)
 
 chat_id = 'chat_id'
-#chat_id2 = 'chat_id2'
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
@@ -25,34 +29,7 @@ def send_welcome(message):
 я ваш бот персональный помощник, что я умею:
   • к сожелению ничего кроме показания погоды(
 """)
-    print("Hello")
-
-#Weather
-#city = 'Ваш город или район'
-#link = f"https://www.google.com/search?q=погода+в+{city}"
-
-#headers = {
-#    "User-Agent" : "Ваш User_Agent"
-#}
-#responce = requests.get(link, headers=headers)
-#print(responce)
-
-#soup = BeautifulSoup(responce.text, "html.parser")
-#print(soup)
-
-# Парсим погоду
-#temperature = soup.select("#wob_tm")[0].getText()
-#title = soup.select("#wob_dc")[0].getText()
-#humidity = soup.select("#wob_hm")[0].getText()
-#time = soup.select("#wob_dts")[0].getText()
-#wind = soup.select("#wob_ws")[0].getText()
-
-# Выводим на экран
-#print(time, "\n",title)
-#print(title)
-#print(f"Температура: {temperature}C")
-#print(f"Влажность: {humidity}")
-#print(f"Ветер: {wind}")
+    logging.info('Подключился новый пользователь!')
 
 def schedule_checker():
     while True:
@@ -69,6 +46,14 @@ def morning():
     }
     responce = requests.get(link, headers=headers)
     print(responce)
+
+    f = '<Response [200]>'
+
+    if str(responce) == str(f):
+        logging.info('Подключение к серверам google прошло успешно ' + '(' + str(responce) + ')')
+    else:
+        logging.error('Поключение к серверам google прошло неудачно, не удалось подключится!' + '(' + str(responce) + ')', exc_info=True)
+
 
     soup = BeautifulSoup(responce.text, "html.parser")
 
@@ -98,17 +83,7 @@ def morning():
 """)
     bot.send_message(chat_id, f'Желаю вам {random.choice(good)} дня!')
     print('Погода отправилась')
-    #bot.send_message(chat_id2, f"""
-#доброе утро, погода на утро в ...:
-#""")
-    #bot.send_message(chat_id2, f"""\
-#дата и время: {time}
-#состояние: {title}
-#температура: {temperature}С
-#влажность: {humidity}
-#ветер: {wind}
-#""")
-    #bot.send_message(chat_id2, f'Желаю вам {random.choice(good)} дня!')
+    logging.info('Погода отправилась')
 
 if __name__ == "__main__":
     schedule.every().day.at("07:00").do(morning)
